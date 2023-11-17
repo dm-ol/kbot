af90143c9f6351661d270c7ad9896be3831ab2ec
Dmytro Oliinyk <devdm.dp@gmail.com>
2023-11-17 22:06:27 +0000

    App v1.0.2

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -51,7 +51,7 @@ to quickly create a Cobra application.`,
 
 			switch payload {
 			case "hello":
-				err = m.Send(fmt.Sprintf("Hello I`m kbot %s!", appversion))
+				err = m.Send(fmt.Sprintf("Hello! I`m Kbot %s!", appversion))
 			}
 
 			return err
